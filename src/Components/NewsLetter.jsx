8710ea9688fcc99041ccf6e5c20e9798b79ec00c
alann03 <alanhuismann03@gmail.com>
2022-10-17 13:17:42 -0300

    input validations

--- src/Components/NewsLetter.jsx
+++ src/Components/NewsLetter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import RightArrow from "../Assets/svg/right-arrow.svg";
 
@@ -45,20 +45,47 @@ const NewsLetterCont = styled.div`
     height: 100%;
     cursor: pointer;
   }
+  .error {
+    margin-top: 1%;
+    color: #e63946;
+  }
 `;
 
 function NewsLetter() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const validate = input => {
+    let error = "";
+    if (!/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/.test(input)) error = "Insert a valid email. For example: foo-bar.baz@example.com";
+    return error;
+  }
+
+  const handleChange = e => {
+    setEmail(e.target.value);
+    setError(validate(e.target.value));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault(e);
+    if (email === "") alert("Email is required");
+    else if (error !== "") alert("Insert a valid email");
+    // En un caso real, aquí enviaríamos el email
+    else alert("Email sent");
+  }
+
   return (
     <NewsLetterCont>
       <h4>NEWSLETTER</h4>
       <h2>SUSCRIBITE</h2>
       <span>Y enterate de todas las novedades</span>
-      <form>
-        <input type="email" placeholder="Ingresa tu email" ></input>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input type="email" placeholder="Ingresa tu email" value={email} onChange={e => handleChange(e)} ></input>
         <button type="submit">
           <img src={RightArrow} alt="" />
         </button>
       </form>
+      <span className="error">⠀{error}</span>
     </NewsLetterCont>
   )
 }
